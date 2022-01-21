7df86e14e540d000f0391d725d845ebb9e1e0d0d
Graeme Haddow <graeme.haddow@bt.com>
2022-01-21 15:15:00 +0000

    Updates

---
+++ precache-manifest.9ac373150d1f5ae439597278cef31e82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de8ba40e690d4f8dcd8ddc3c35f623ee",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "a7dfe941243b6a3c8568",
+    "url": "/portfoliosite/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "91ac9c8bedb1f3b78d72",
+    "url": "/portfoliosite/static/css/main.460a819f.chunk.css"
+  },
+  {
+    "revision": "a7dfe941243b6a3c8568",
+    "url": "/portfoliosite/static/js/2.efd39910.chunk.js"
+  },
+  {
+    "revision": "0db18f2f56e579bdbf0e639ec3e0b99a",
+    "url": "/portfoliosite/static/js/2.efd39910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ac9c8bedb1f3b78d72",
+    "url": "/portfoliosite/static/js/main.fa04b5d7.chunk.js"
+  },
+  {
+    "revision": "6cdf31383e7e4ab6f269",
+    "url": "/portfoliosite/static/js/runtime-main.fa514bd4.js"
+  },
+  {
+    "revision": "b8798ab45185fbddedcd2700d86492fe",
+    "url": "/portfoliosite/static/media/me.b8798ab4.png"
+  }
+]);

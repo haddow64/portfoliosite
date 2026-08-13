fc6f7d5652ec6ec2516430f0af80bcd244b89159
Graeme Haddow <graeme.haddow@diageo.com>
2026-08-13 09:42:49 +0100

    Update CV download link

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,5 @@
 export const portfolioLinks = {
-  cv: "https://github.com/haddow64/CV/raw/main/Graeme_Haddow_-_Senior_Software_Engineer_green.pdf",
+  cv: "https://github.com/haddow64/CV/raw/main/Graeme_Haddow_-_Lead_Software_Engineer_green.pdf",
   github: "https://github.com/haddow64",
   linkedin: "https://www.linkedin.com/in/ghaddow64/",
   email: "mailto:graeme@haddow64.com",
